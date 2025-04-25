8c547699d5f3c4d2bc6a19c2f66012263976f032
Israel Campiotti <israelcampiotti@gmail.com>
2025-04-25 10:04:01 -0300

    add day 13, linear equations for the win

--- 2024/day13/solution.py
+++ 2024/day13/solution.py
@@ -16,6 +16,9 @@ class Game:
     x: int
     y: int
 
+A_PRICE = 3
+B_PRICE = 1
+
 
 def read_input(file: str) -> list[Game]:
     data = Path(file).read_text()
@@ -26,15 +29,45 @@ def read_input(file: str) -> list[Game]:
         games.append(Game(a=Button(dx=adx, dy=ady), b=Button(dx=bdx, dy=bdy), x=x, y=y))
     return games
 
+def solve_game(game: Game) -> int:
+    a = game.a
+    b = game.b
+
+    left_side = a.dx - a.dy - (a.dx / b.dx) * (b.dx - b.dy)
+    right_side = game.x - game.y - (game.x / b.dx) * (b.dx - b.dy)
+
+    na = right_side / left_side
+    nb = (game.x - na * a.dx) / (b.dx)
+
+    if na < 0 or nb < 0:
+        return 0
+    
+    rounded_a, rounded_b = round(na), round(nb)
+    is_a_close = abs(rounded_a - na) < 1e-3
+    is_b_close = abs(rounded_b - nb) < 1e-3
+
+    if not is_a_close or not is_b_close:
+        return 0
+    
+    return rounded_a * A_PRICE + rounded_b * B_PRICE
+
 
 if __name__ == "__main__":
-    file = "./sample.txt"
+    file = "./input.txt"
     games = read_input(file)
 
-    current_game = games[0]
+    result = 0
+    for game in games:
+        r = solve_game(game)
+        result += r
 
-    # na * a.dx + nb * b.dx = x
-    # na * a.dy + nb * b.dy = y
-    # na * (a.dx - a.dy) + nb * (b.dx - b.dy) = x - y
+    print("PART ONE", int(result))
 
-    print(games)
+    result = 0
+    for game in games:
+        game.x = 10000000000000 + game.x
+        game.y = 10000000000000 + game.y
+        r = solve_game(game)
+        result += r
+
+    print("PART TWO", int(result))
