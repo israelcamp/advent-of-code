ddfe60ae501ea679691aeafbc0178e6f4ec9d4a1
Israel Campiotti <israelcampiotti@gmail.com>
2025-05-02 10:50:05 -0300

    day 18 done

--- 2024/day18/solution.py
+++ 2024/day18/solution.py
@@ -11,35 +11,9 @@ def read_input(file: str):
     return positions
 
 
-if __name__ == "__main__":
-    import sys
-
-    filename = sys.argv[1]
-    file = f"{filename}.txt"
-
-    if filename.startswith("sample"):
-        N, FALLEN_BYTES = 7, 12
-    else:
-        N = 71
-        FALLEN_BYTES = 1024
-
+def solution_part_one(fallen_bytes: list[tuple[int, int]], N, target_row: int, target_col: int) -> int:
     SOLUTION_SCORE = 1_000_000
     SOLUTION_STEP = None
-
-    bytes_positions = read_input(file)
-    bytes_positions = bytes_positions[:FALLEN_BYTES]
-
-    fallen_grid = []
-    for i in range(N):
-        row = []
-        for j in range(N):
-            if (i, j) in bytes_positions:
-                row.append("#")
-            else:
-                row.append(".")
-        fallen_grid.append(row)
-
-    Path("grid.txt").write_text("\n".join("".join(row) for row in fallen_grid))
 
     directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
     memo = {}
@@ -58,7 +32,7 @@ if __name__ == "__main__":
                 if next_row < 0 or next_row >= N or next_col < 0 or next_col >= N:
                     continue
 
-                if (next_row, next_col) in bytes_positions:
+                if (next_row, next_col) in fallen_bytes:
                     continue
 
                 next_score = step["score"] + 1
@@ -75,7 +49,7 @@ if __name__ == "__main__":
                     row=next_row, col=next_col, next=step, score=next_score
                 )
 
-                if (next_row, next_col) == (0, 0):
+                if (next_row, next_col) == (target_row, target_col):
                     SOLUTION_SCORE = next_score
                     SOLUTION_STEP = next_step
                     continue
@@ -84,4 +58,48 @@ if __name__ == "__main__":
 
         current_steps = next_steps
 
-    print(SOLUTION_SCORE)
+    return SOLUTION_STEP
+
+
+if __name__ == "__main__":
+    import sys
+
+    filename = sys.argv[1]
+    file = f"{filename}.txt"
+
+    if filename.startswith("sample"):
+        N, FALLEN_BYTES = 7, 12
+    else:
+        N = 71
+        FALLEN_BYTES = 1024
+
+    SOLUTION_SCORE = 1_000_000
+    SOLUTION_STEP = None
+
+    bytes_positions = read_input(file)
+    fallen_bytes_grid = bytes_positions[:FALLEN_BYTES]
+
+    solution_step = solution_part_one(fallen_bytes_grid, N, 0, 0)
+    solution_potisions = []
+    step = solution_step
+    while step is not None:
+        solution_potisions.append((step["row"], step["col"]))
+        step = step["next"]
+
+    while FALLEN_BYTES < len(bytes_positions):
+        next_fallen_byte = FALLEN_BYTES + 1
+        if bytes_positions[next_fallen_byte] not in solution_potisions:
+            FALLEN_BYTES += 1
+            continue
+
+        solution_step = solution_part_one(bytes_positions[:next_fallen_byte+1], N, 0, 0)
+
+        if solution_step is None:
+            print("STOP", bytes_positions[next_fallen_byte])
+            break
+
+        solution_potisions = []
+        step = solution_step
+        while step is not None:
+            solution_potisions.append((step["row"], step["col"]))
+            step = step["next"]
