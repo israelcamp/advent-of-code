8273e4265c9e5b6d9b667c1a67bc084de4dca87d
Israel Campiotti <israelcampiotti@gmail.com>
2025-05-04 19:27:08 -0300

    day 21 solution p1

--- 2024/day21/solution.py
+++ 2024/day21/solution.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 def read_input(file: str):
     data = Path(file).read_text().splitlines()
     return [list(l) for l in data]
@@ -17,82 +18,114 @@ NUMPAD = {
     "3": (2, 2),
     "0": (3, 1),
     "A": (3, 2),
-    "#": (3, 0)
+    "#": (3, 0),
 }
 
-MOVEMENTS = {
-    (-1, 0): "^",
-    (1, 0): "v",
-    (0, -1): "<",
-    (0, 1): ">",
-    (0, 0): ""
-}
+MOVEMENTS = {(-1, 0): "^", (1, 0): "v", (0, -1): "<", (0, 1): ">", (0, 0): ""}
 
-DIRPAD = {
-    "^": (0, 1),
-    "A": (0, 2),
-    "<": (1, 0),
-    "v": (1, 1),
-    ">": (1, 2),
-    "#": (0, 0)
-}
+DIRPAD = {"^": (0, 1), "A": (0, 2), "<": (1, 0), "v": (1, 1), ">": (1, 2), "#": (0, 0)}
 
-def solve_sequence(start_button: str, pos_dict: dict, current_sequence: str) -> str:
-    start_position = pos_dict[start_button]
-    inverted_pos_dict = {v:k for k,v in pos_dict.items()}
 
-    movement_sequence = ""
-    for next_button in current_sequence:
-        next_position = pos_dict[next_button]
+def solve_sequence(current_button: str, next_button: str, pos_dict: dict) -> set:
+    inverted_pos_dict = {v: k for k, v in pos_dict.items()}
 
-        distance_row = next_position[0] - start_position[0]
-        distance_col = next_position[1] - start_position[1]
+    start_position = pos_dict[current_button]
+    next_position = pos_dict[next_button]
 
-        row_norm = max(abs(distance_row), 1)
-        col_norm = max(abs(distance_col), 1)
-        direction_row = distance_row // row_norm
-        direction_col = distance_col // col_norm
+    distance_row = next_position[0] - start_position[0]
+    distance_col = next_position[1] - start_position[1]
 
-        if inverted_pos_dict[(next_position[0], 0)] == "#":
-            preference = "col"
-        elif inverted_pos_dict[(0, next_position[1])] == "#":
-            preference = "row"
-        else:
-            preference = "row"
+    row_norm = max(abs(distance_row), 1)
+    col_norm = max(abs(distance_col), 1)
+    direction_row = distance_row // row_norm
+    direction_col = distance_col // col_norm
 
-        if preference == "row":
-            movement_sequence += row_norm * MOVEMENTS[(direction_row, 0)] + col_norm * MOVEMENTS[(0, direction_col)]
-        else:
-            movement_sequence += col_norm * MOVEMENTS[(0, direction_col)] + row_norm * MOVEMENTS[(direction_row, 0)]
-        movement_sequence += "A"
-        
-        start_button = next_button
-        start_position = next_position
+    directions = ["row", "col"]
 
-    return movement_sequence
+    if inverted_pos_dict[(next_position[0], start_position[1])] == "#":
+        directions.remove("row")
+    if inverted_pos_dict[(start_position[0], next_position[1])] == "#":
+        directions.remove("col")
+
+    possible_paths = set()
+    for dir in directions:
+        if dir == "row":
+            possible_paths.add(
+                row_norm * MOVEMENTS[(direction_row, 0)]
+                + col_norm * MOVEMENTS[(0, direction_col)]
+                + "A"
+            )
+        if dir == "col":
+            possible_paths.add(
+                col_norm * MOVEMENTS[(0, direction_col)]
+                + row_norm * MOVEMENTS[(direction_row, 0)]
+                + "A"
+            )
+
+    return possible_paths
+
+
+def solve_robot2(sequence: str):
+    current_sequence = "A" + sequence
+    result = 0
+    for i in range(0, len(current_sequence) - 1):
+        current_button = current_sequence[i]
+        next_button = current_sequence[i + 1]
+        possible_paths = solve_sequence(
+            current_button=current_button, next_button=next_button, pos_dict=DIRPAD
+        )
+        result += min(len(p) for p in possible_paths)
+    return result
+
+
+def solve_robot1(sequence: str) -> int:
+
+    current_sequence = "A" + sequence
+    result = 0
+    for i in range(0, len(current_sequence) - 1):
+        current_button = current_sequence[i]
+        next_button = current_sequence[i + 1]
+        possible_paths = solve_sequence(
+            current_button=current_button, next_button=next_button, pos_dict=DIRPAD
+        )
+
+        solutions = []
+        for path in possible_paths:
+            _solution = solve_robot2(path)
+            solutions.append(_solution)
+
+        result += min(solutions)
+
+    return result
+
 
 if __name__ == "__main__":
 
     import sys
+
     _filename = sys.argv[1]
     filename = f"{_filename}.txt"
 
     sequences = read_input(filename)
-    
-    sequences = [sequences[-1]]
 
+    global_result = 0
 
-    results = 0
-    for current_sequence in sequences:
-        numpad_sequence = solve_sequence(start_button="A", pos_dict=NUMPAD, current_sequence=current_sequence,)
-        robot1_sequence = solve_sequence(start_button="A", pos_dict=DIRPAD, current_sequence=numpad_sequence, )
-        robot2_sequence = solve_sequence(start_button="A", pos_dict=DIRPAD, current_sequence=robot1_sequence, )
-        print(len(robot2_sequence), "".join(current_sequence))
-        results += len(robot2_sequence) * int("".join(current_sequence[:-1]))
+    for sequence in sequences:
+        current_sequence = ["A"] + sequence
+        result = 0
+        for i in range(0, len(current_sequence) - 1):
+            current_button = current_sequence[i]
+            next_button = current_sequence[i + 1]
+            possible_paths0 = solve_sequence(
+                current_button=current_button, next_button=next_button, pos_dict=NUMPAD
+            )
 
-        # break
+            solutions = []
+            for path in possible_paths0:
+                _solution = solve_robot1(path)
+                solutions.append(_solution)
 
-    print(results)
-    print(robot2_sequence)
-    print(robot1_sequence)
-    print(numpad_sequence)
+            result += min(solutions)
+        global_result += result * int("".join(sequence[:-1]))
+
+    print(global_result)
