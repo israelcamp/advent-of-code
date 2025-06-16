4830a342d331a6b857ad4216d77723a2a87cba9d
Israel Campiotti <israelcampiotti@gmail.com>
2025-06-16 19:00:51 -0300

    day 6 done with maths!

---
+++ 2023/day6/part2.lua
@@ -0,0 +1,39 @@
+local file = io.open("input.txt", "r")
+
+if not file then
+	print("Could not open file")
+	return
+end
+
+local times = {}
+local distances = {}
+local line_count = 0
+for line in file:lines() do
+	local number = ""
+	for num in line:gmatch("%d+") do
+		number = number .. num
+	end
+	if line_count == 0 then
+		table.insert(times, tonumber(number))
+	elseif line_count == 1 then
+		table.insert(distances, tonumber(number))
+	else
+		print("Unexpected line in file: " .. line)
+	end
+	line_count = line_count + 1
+end
+file:close()
+
+local total = 1
+for i = 1, #times do
+	local T = times[i]
+	local d = distances[i]
+	local delta = math.sqrt(T * T - 4 * d)
+	local h1 = math.ceil(((T + delta) / 2) - 1)
+	local h2 = math.floor(((T - delta) / 2) + 1)
+	local solutions = h1 - h2 + 1
+	total = total * solutions
+	print("For T = " .. T .. " and d = " .. d .. ", h1 = " .. h1 .. ", h2 = " .. h2 .. ", solutions = " .. solutions)
+end
+
+print("Total number of solutions: " .. total)
